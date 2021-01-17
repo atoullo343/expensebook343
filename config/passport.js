e757df41f46ed692c45afbd1928b1e05dc5077a3
atoullo <atoulloraimov@gmail.com>
2021-01-17 11:11:40 +0500

    dddd

--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,7 @@ module.exports = function (passport) {
             {
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: '/auth/google/callback',
-                proxy: true
+                callbackURL: '/auth/google/callback'
             },
             async (accessToken, refreshToken, profile, done) => {
                 const newUser = {
